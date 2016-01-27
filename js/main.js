e4adf47bf1cd554c384808531a3faa7831405021
Nikhil <nikhilsethi108@gmail.com>
2016-01-27 10:56:11 -0500

    redesign

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    // var picCount = 0;
+    // var picArray = ["pics/DSC03089.jpg", "pics/DSC03567.jpg", "pics/DSC03648.jpg", "pics/DSC04007.jpg",
+    // "pics/DSC04435.jpg", "pics/DSC04459.jpg", "pics/DSC04606.jpg", "pics/DSC04716.jpg", "pics/DSC05201.jpg",
+    // "pics/DSC05330.jpg"];
+
+    // var html = '<img id="background" src="' + picArray[3] + '">';
+    // console.log(html);
+
+    // $('#image').html(html);
+
+});
